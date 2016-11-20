d2fb67ecbf9c43978c46d39dc03fa50760d87dce
Marc Brinkmann <git@marcbrinkmann.de>
2016-11-20 21:30:52 +0100

    Skeleton for observer test case.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::rc::Weak;
+use std::rc::{Rc, Weak};
 
 
 pub struct WeakVec<T: ?Sized>(Vec<Weak<T>>);
@@ -44,8 +44,9 @@ impl<T> Multiplexer<T> {
         Multiplexer { listeners: WeakVec::new(Vec::new()) }
     }
 
-    pub fn register(&mut self, listener: Weak<Observer<Item = T>>) {
-        self.listeners.push(listener);
+    #[inline]
+    pub fn register(&mut self, listener: Rc<Observer<Item = T>>) {
+        self.listeners.push(Rc::downgrade(&listener))
     }
 
     pub fn distribute(&self, mut item: T) -> Option<T> {
@@ -68,8 +69,51 @@ pub trait Observer {
     fn handle(&self, item: Self::Item) -> Option<Self::Item>;
 }
 
+
 #[cfg(test)]
 mod tests {
+    use std::rc::Rc;
+    use super::*;
+
+    struct Num(pub u32);
+
+    struct NumWaiter {
+        pub n: u32,
+        pub found: bool,
+    }
+
+    impl NumWaiter {
+        fn new(n: u32) -> NumWaiter {
+            NumWaiter {
+                n: n,
+                found: false,
+            }
+        }
+    }
+
+    impl Observer for NumWaiter {
+        type Item = Num;
+
+        fn handle(&self, item: Num) -> Option<Num> {
+            if self.n != item.0 {
+                Some(item)
+            } else {
+                // consume if number matches
+                None
+            }
+        }
+    }
+
     #[test]
-    fn it_works() {}
+    fn basic_observer() {
+        let mut mp = Multiplexer::new();
+
+        let w_1 = Rc::new(NumWaiter::new(1));
+        let w_7 = Rc::new(NumWaiter::new(7));
+        let w_99 = Rc::new(NumWaiter::new(9));
+
+        mp.register(w_1.clone());
+        mp.register(w_7.clone());
+        mp.register(w_99.clone());
+    }
 }
